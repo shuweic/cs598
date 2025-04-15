f760fe39eddfdc4db0e6c7f66fd6d7c45b229068
shuweic <shuweic227@gmail.com>
2025-04-15 17:01:14 -0500

    Remove ignored files from repo (already tracked)

--- temp_10MB.csv.sql
+++
@@ -1,7 +0,0 @@
-
-    SET enable_profiling = JSON;
-    SET profiling_output = './profile_output/profile_10MB.csv.json';
-
-    DROP TABLE IF EXISTS t;
-    CREATE TABLE t AS SELECT * FROM read_csv_auto('./data/10MB.csv');
-    
